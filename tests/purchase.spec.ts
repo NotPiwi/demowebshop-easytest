d9ac460786dbc167e1bf21f1839ce258af91c0bf
NotPiwi <piwineitor1@gmail.com>
2024-12-13 20:46:07 -0700

    Added 8 items for Apple and Samsung in different colors
    White Apple Cover added
    Black Apple Cover added
    Blue Apple Cover added
    Yellow Apple Cover added
    White Samsung Cover added
    Black Samsung Cover added
    Blue Samsung Cover added
    Yellow Samsung Cover added

--- tests/purchase.spec.ts
+++ tests/purchase.spec.ts
@@ -1,6 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('test-PURCHASE', async ({ page }) => {
+  //Productos a añadir al carrito
+  const products = [
+    { brand: 'Apple', color: 'White' },
+    { brand: 'Apple', color: 'Black' },
+    { brand: 'Apple', color: 'Blue' },
+    { brand: 'Apple', color: 'Yellow' },
+    { brand: 'Samsung', color: 'White' },
+    { brand: 'Samsung', color: 'Black' },
+    { brand: 'Samsung', color: 'Blue' },
+    { brand: 'Samsung', color: 'Yellow' },
+  ];
+
   await page.goto('https://demowebshop.tricentis.com/');
   await page.getByRole('link', { name: 'Log in' }).click();
   await page.getByLabel('Email:').click();
@@ -11,10 +23,17 @@ test('test-PURCHASE', async ({ page }) => {
   await page.getByRole('link', { name: 'Electronics' }).first().click();
   await page.getByRole('heading', { name: 'Cell phones' }).getByRole('link').click();
   await page.getByRole('link', { name: 'Phone Cover', exact: true }).click();
-  await page.getByLabel('Qty:').click();
-  await page.getByLabel('Qty:').fill('2');
-  await page.locator('#add-to-cart-button-80').click();
-  await page.getByRole('link', { name: 'Shopping cart' }).click();
+  
+  //Ciclo para añadir cada producto al carrito
+  for (const product of products) {
+    const brandDropdown = await page.locator('select[id="product_attribute_80_2_37"]');
+    await brandDropdown.selectOption(product.brand);
+    const colorDropdown = await page.locator('select[id="product_attribute_80_1_38"]');
+    await colorDropdown.selectOption(product.color);
+    await page.locator('#add-to-cart-button-80').click();
+    expect(await page.waitForSelector('text=The product has been added to your shopping cart', { state: 'visible' })).toBeTruthy();
+  }
+  await page.getByRole('link', { name: 'Shopping cart' }).first().click();
   await page.locator('#termsofservice').check();
   await page.locator('.checkout-buttons').click();
   for (let i = 0; i < 5; i++) { //aqui hice un ciclo porque eran 5 "Continue" seguidos
